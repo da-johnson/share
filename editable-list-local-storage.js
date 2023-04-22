6754ceaac20f243bbc25c1254249f1f555b1daf1
Alex Johnson <91097733+da-johnson@users.noreply.github.com>
2023-04-21 19:53:31 -0700

    Delete editable-list-local-storage.js

--- editable-list-local-storage.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	if (localStorage.getItem('myTasksData')) {
-		$("#myTasks").html(localStorage.getItem('myTasksData'));
-	}
-
-	$("#myTasks").on('input', function() {
-		localStorage.setItem('myTasksData', this.innerHTML);
-	});
-});
